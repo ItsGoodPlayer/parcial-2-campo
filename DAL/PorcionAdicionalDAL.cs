e84d0f0f71793c9a2cf583c8dc4033d9fb3460f0
nachi <nachi.gg@hotmail.com>
2025-07-03 14:01:20 -0300

    fixes

--- DAL/PorcionAdicionalDAL.cs
+++ DAL/PorcionAdicionalDAL.cs
@@ -52,25 +52,6 @@ namespace DAL
             return null;
         }
 
-        public PorcionAdicional ObtenerPorcionPorTipo(TipoPorcion tipo)
-        {
-            string sql = "SELECT Id, Tipo, Precio FROM PorcionesAdicionales WHERE Tipo = @Tipo";
-            var parametros = new List<SqlParameter> { CrearParametro("@Tipo", tipo.ToString()) };
-            
-            DataTable tabla = Leer(sql, parametros);
-
-            if (tabla.Rows.Count > 0)
-            {
-                DataRow row = tabla.Rows[0];
-                return new PorcionAdicional(tipo, Convert.ToDecimal(row["Precio"]))
-                {
-                    Id = Convert.ToInt32(row["Id"])
-                };
-            }
-
-            return null;
-        }
-
         public int ObtenerIdPorcion(TipoPorcion tipo)
         {
             string sql = "SELECT Id FROM PorcionesAdicionales WHERE Tipo = @Tipo";
@@ -78,65 +59,6 @@ namespace DAL
             
             object resultado = EscribirEscalar(sql, parametros);
             return resultado != null ? Convert.ToInt32(resultado) : 0;
-        }
-
-        public decimal ObtenerPrecioPorcion(TipoPorcion tipo)
-        {
-            string sql = "SELECT Precio FROM PorcionesAdicionales WHERE Tipo = @Tipo";
-            var parametros = new List<SqlParameter> { CrearParametro("@Tipo", tipo.ToString()) };
-            
-            object resultado = EscribirEscalar(sql, parametros);
-            return resultado != null ? Convert.ToDecimal(resultado) : 0m;
-        }
-
-        public bool ActualizarPrecioPorcion(TipoPorcion tipo, decimal nuevoPrecio)
-        {
-            string sql = "UPDATE PorcionesAdicionales SET Precio = @Precio WHERE Tipo = @Tipo";
-            var parametros = new List<SqlParameter> 
-            { 
-                CrearParametro("@Precio", nuevoPrecio),
-                CrearParametro("@Tipo", tipo.ToString()) 
-            };
-            
-            int resultado = Escribir(sql, parametros);
-            return resultado > 0;
-        }
-
-        public bool ExistePorcion(TipoPorcion tipo)
-        {
-            string sql = "SELECT COUNT(*) FROM PorcionesAdicionales WHERE Tipo = @Tipo";
-            var parametros = new List<SqlParameter> { CrearParametro("@Tipo", tipo.ToString()) };
-            
-            object resultado = EscribirEscalar(sql, parametros);
-            return resultado != null && Convert.ToInt32(resultado) > 0;
-        }
-
-        public List<PorcionAdicional> ObtenerPorcionesPorPedido(int pedidoId)
-        {
-            List<PorcionAdicional> porciones = new List<PorcionAdicional>();
-            
-            string sql = @"SELECT pa.Id, pa.Tipo, pa.Precio, pp.Cantidad
-                          FROM PedidoPorciones pp
-                          INNER JOIN PorcionesAdicionales pa ON pp.PorcionId = pa.Id
-                          WHERE pp.PedidoId = @PedidoId";
-
-            var parametros = new List<SqlParameter> { CrearParametro("@PedidoId", pedidoId) };
-            DataTable tabla = Leer(sql, parametros);
-
-            foreach (DataRow row in tabla.Rows)
-            {
-                TipoPorcion tipo;
-                Enum.TryParse(row["Tipo"].ToString(), out tipo);
-
-                var porcion = new PorcionAdicional(tipo, Convert.ToDecimal(row["Precio"]))
-                {
-                    Id = Convert.ToInt32(row["Id"]),
-                    Cantidad = Convert.ToInt32(row["Cantidad"])
-                };
-                porciones.Add(porcion);
-            }
-
-            return porciones;
         }
     }
 }
